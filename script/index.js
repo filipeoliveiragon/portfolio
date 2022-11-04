45fd6e95b915abe4a6760af8376528981f26838a
Luiz Filipe de Oliveira Gonçalves <filipeoliveira@arpiatecnologia.com.br>
2022-11-04 18:32:45 -0300

    Tela Home

---
+++ script/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    if(!$("#myCanvas").tagcanvas({
+        textColour: "#FFF",
+        fontWeight: "800",
+        fontSize: 30,
+        outlineColour: "transparent",
+        reverse: true,
+        depth: 0.8,
+        maxSpeed: 0.05,
+        weigth: true,
+    }, "tags")){
+        $("#myCanvasContainer")
+    }
+})
